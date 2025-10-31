76a7c0cbfe9e47c281d833ea84353330e926175d
tanjil <tanjilh84@gmail.com>
2025-10-31 22:16:24 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strip unknown properties
+      forbidNonWhitelisted: true, // throw if unknown props are present (optional)
+      transform: true, // auto-transform payloads to DTO instances
+    }),
+  );
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
